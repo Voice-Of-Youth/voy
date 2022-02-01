21c166490b21fb0512c3e7623d1445225da0d0ff
nmktad <nmktadesse@gmail.com>
2022-02-01 16:30:09 +0300

    Modified the folder structure and changed the navbar and footer elements

---
+++ src/js/data/data.js
@@ -0,0 +1,47 @@
+const faq = [
+    {
+        id: "faq1",
+        title: "How can i be a writer",
+        tags: ["writers", "write", "how to"],
+        count: 0,
+        answer: "You can be a writer by signing up as a writer and then filling out the forms for verifying who you are, after the verification process you can write on this platform. you'll also get a certificate indicating you are a writer in this platform",
+    }
+]
+
+const articles = [
+    {
+        id: "art1",
+        title: "How culture affects sexual education",
+        tags: [],
+        content: {
+            images: [],
+            paragraphs: []
+        }
+    }
+]
+
+const users = {
+    readers: [
+        {
+            fullName: "Nathnael Mekonnen",
+            userName: "nmktad",
+            password: "nmktad2090",
+            list: {
+                articles: [],
+                following: [],
+            }
+        }
+    ],
+
+    writers: [
+        {
+            fullName: "Nahom temam",
+            userName: "nahtm",
+            password: "nahtm1915",
+            list: {
+                articles: {},
+                following: {},
+            }
+        }
+    ]
+}
