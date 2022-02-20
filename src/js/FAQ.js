ae597b57bf9fd9515f5f69fc479d61836fe8d6d9
nmktad <nmktadesse@gmail.com>
2022-02-20 20:36:54 +0300

    Fixed bug on faq page and modified styles associated to this page

--- src/js/FAQ.js
+++ src/js/FAQ.js
@@ -1,8 +1,7 @@
 const faqs = document.querySelectorAll(".faq");
 
-faqs.forEach( (faq) => {
+faqs.forEach((faq) => {
     faq.addEventListener("click", () => {
         faq.classList.toggle("active");
     });
 });
-
